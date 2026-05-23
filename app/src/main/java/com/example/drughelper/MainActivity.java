22c8473d2ae42bc5e490549eafb0e644cc88458b
kyumin <kyumin1429@naver.com>
2026-05-24 01:56:07 +0900

    first design and first day

--- app/src/main/java/com/example/drughelper/MainActivity.java
+++ app/src/main/java/com/example/drughelper/MainActivity.java
@@ -7,14 +7,19 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.RecyclerView;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends AppCompatActivity implements MyAdapter.ItemClickListener {
+    MyAdapter adapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+        RecyclerView recyclerView = findViewById(R.id.timeMedicine);
+
+        recyclerView.setAdapter(adapter);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
---
+++ app/src/main/java/com/example/drughelper/MyAdapter.java
@@ -0,0 +1,7 @@
+package com.example.drughelper;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+public class MyAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+
+}

aab15a11ff67c839a0cf71c800f311fe0d8cbf67
kyumin <kyumin1429@naver.com>
2026-05-26 14:14:08 +0900

    main button activate

--- app/src/main/java/com/example/drughelper/MainActivity.java
+++ app/src/main/java/com/example/drughelper/MainActivity.java
@@ -21,6 +21,8 @@ public class MainActivity extends AppCompatActivity implements MyAdapter.ItemCli
     private Button button1;
     private Button button2;
     private Button button3;
+    private Button button4;
+    private View viewFab;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,10 +31,11 @@ public class MainActivity extends AppCompatActivity implements MyAdapter.ItemCli
         String[] data = new String[5];
         for (int i=1; i <= 5; i++) { data[i-1] = "약 이름"+i; }
 
-        button1 = findViewById(R.id.create);
+        button1 = findViewById(R.id.record);
         button2 = findViewById(R.id.logout2);
-        button3 = findViewById(R.id.logout2);
-
+        button3 = findViewById(R.id.statics);
+        button4 = findViewById(R.id.home);
+        viewFab = findViewById(R.id.fab);
         RecyclerView recyclerView = findViewById(R.id.timeMedicine);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
         adapter = new MyAdapter(this,data);
@@ -63,7 +66,9 @@ public class MainActivity extends AppCompatActivity implements MyAdapter.ItemCli
         button1.setOnClickListener(listener);
         button2.setOnClickListener(listener);
         button3.setOnClickListener(listener);
-        if (id == R.id.create) {
+        button4.setOnClickListener(listener);
+        viewFab.setOnClickListener(listener);
+        if (id == R.id.statics) {
             Intent intent = new Intent(MainActivity.this, StatsActivity2.class);
             startActivity(intent);
         }
@@ -77,6 +82,18 @@ public class MainActivity extends AppCompatActivity implements MyAdapter.ItemCli
             Intent intent = new Intent(MainActivity.this, LoginActivity.class);
             intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
 
+            startActivity(intent);
+        }
+        else if (id == R.id.home) {
+            Intent intent = new Intent(MainActivity.this, MainActivity.class); // 자기 자신 호출
+
+            // 중요: 기존에 켜져 있던 메인 화면을 위로 올리고, 그 위에 쌓인 다른 화면이 있다면 제거합니다.
+            intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_SINGLE_TOP);
+
+            startActivity(intent);
+        }
+        else if (id == R.id.record) {
+            Intent intent = new Intent(MainActivity.this, RecordActivity.class);
             startActivity(intent);
         }
     }
